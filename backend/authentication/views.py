13c89db26dadea53250e14d56dd30b1c048ff2ac
sanju <sanjuraj57421@gmail.com>
2025-02-25 23:20:45 +0530

    add auth

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.serializers import ModelSerializer
 
-# Create your views here.
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)  
+        user.save()
+
+token_obtain_view = TokenObtainPairView.as_view()
+token_refresh_view = TokenRefreshView.as_view()
+
