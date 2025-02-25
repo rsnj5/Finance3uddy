62120bd345ab7013c4d0f8f20945f8a46585a1b7
sanju <sanjuraj57421@gmail.com>
2025-02-25 21:17:30 +0530

    add files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -15,4 +13,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
