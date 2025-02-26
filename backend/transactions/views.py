6638a2225a24f8477415b8e9de093ab0c4c8dd75
krishagarwal0109 <krish.agarwal.ece23@itbhu.ac.in>
2025-02-26 13:07:02 +0530

    add transaction

--- backend/transactions/views.py
+++ backend/transactions/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Transaction
+from .serializers import TransactionSerializer
+from .permissions import IsOwner
 
-# Create your views here.
+class TransactionListCreateView(generics.ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TransactionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
