8238627862cc0282cf50a1e47b0df71851251e39
Sanju Raj <153303834+rsnj5@users.noreply.github.com>
2025-02-26 13:16:14 +0530

    Delete finance3uddy/backend directory

--- finance3uddy/backend/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterUserView, token_obtain_view, token_refresh_view
-
-urlpatterns = [
-    path('signup/', RegisterUserView.as_view(), name='signup'),
-    path('login/', token_obtain_view, name='login'),
-    path('refresh/', token_refresh_view, name='token_refresh'),
-]
--- finance3uddy/backend/transactions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TransactionListCreateView, TransactionRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('', TransactionListCreateView.as_view(), name='transaction-list-create'),
-    path('<int:pk>/', TransactionRetrieveUpdateDestroyView.as_view(), name='transaction-detail'),
-]
-
